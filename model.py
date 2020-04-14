2f84fdd5109e274df9378698e4282adc00c5b647
Michael Hodges <hodges.m@husky.neu.edu>
2020-04-14 17:16:19 -0400

    creating final presentation

--- model.py
+++ model.py
@@ -1,5 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as functional
 
 
+class SimpleNet(nn.module):
+	def __init__(self):
+		super(SimpleNet, self).__init__()
 
-def test_func():
-	print("hello")
+	def forward(self, x):
+
+
+if __name__ == '__main__':
+	pass
