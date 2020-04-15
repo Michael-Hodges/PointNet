4fe549c251b4884b2a804f0741f19a27f76a6945
Michael Hodges <hodges.m@husky.neu.edu>
2020-04-15 18:17:29 -0400

    building dataloader

--- data.py
+++
@@ -1,5 +0,0 @@
-import torch
-import torch.utils.data as data
-
-if __name__ == '__main__':
-	pass
--- model/dataloading.py
+++ model/dataloading.py
@@ -1,2 +1,30 @@
+import os
+import os.path
+import torch
+import torch.utils.data as data
+import numpy as np
+
+
+class ShapeNetClassify(data.Dataset):
+	def __init__(self, path):
+		self.path = path  # path to data folder of shapenet
+		self.classes = {} # dict of the form {class_number, class_name}
+		self.classfile = os.path.join(self.path, 'class_labels.txt')  # create path to file that contains class numbners and labels
+		with open(self.classfile, 'r') as f:
+			for line in f:
+				line = line.strip().split()
+				self.classes[line[1]] = line[0]
+		print(self.classes)
+		for v, k in self.classes.items():
+			print(v, k)
+		
+
+	def __getitem__(self, index):
+		pass
+
+	def __len__():
+		pass
+
+
 if __name__ == '__main__':
-	pass
+	classify_net = ShapeNetClassify('ShapeNet')
